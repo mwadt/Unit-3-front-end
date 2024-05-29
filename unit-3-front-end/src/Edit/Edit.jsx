d4eb503c9518971bd5d1a74e5dc05c9fc6811fc0
mwadt <mbrincko93@gmail.com>
2024-05-29 08:49:10 -0700

    fixed edit

--- unit-3-front-end/src/Edit/Edit.jsx
+++ unit-3-front-end/src/Edit/Edit.jsx
@@ -1,5 +1,66 @@
-export default function Edit() {
+import { useState } from "react";
+
+function EditRecipe({ recipe, handleUpdate}) {
+
+    const [form, setForm] = useState({
+        name: recipe.name,
+        ingredients: recipe.ingredients,
+        instructions: recipe.instructions,
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
     return (
-        <div>hello world</div>
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                handleUpdate(formData);
+            }}
+        >
+            <label htmlFor="name">Name</label>
+            <input
+                type="text"
+                name="name"
+                id="name"
+                value={formData.name}
+                onChange={handleChange}
+            />
+
+            <label htmlFor="ingredients">Ingredients</label>
+            <input
+                type="text"
+                name="ingredients"
+                id="ingredients"
+                value={formData.ingredients}
+                onChange={handleChange}
+            />
+
+            <label htmlFor="instructions"> Instructions: </label>
+            <input
+                type="text"
+                name="instructions"
+                value={formData.instructions}
+                onChange={handleChange}
+            />
+
+            <button type="submit">Update Recipe</button>
+        </form>
+    );
+}
+
+
+
+export default function Edit() {
+    const [recipe, setRecipe] = useState({
+        name: "",
+        ingredients: "",
+        instructions: "",
+    });
+    })
+    
+    return (
+        
     )
 }
